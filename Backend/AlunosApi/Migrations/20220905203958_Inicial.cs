ac09b9f1e827c76ebf3731c50e6e71b8efa4224e
Leo Campos <leopcampos85@gmail.com>
2022-09-06 15:13:53 -0300

    Implementada a funcionalidade de EDIT no frontend

---
+++ Backend/AlunosApi/Migrations/20220905203958_Inicial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlunosApi.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ALUNOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ALUNOS", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ALUNOS",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 1, "mariapenha@yahoo.com", 23, "Maria da Penha" });
+
+            migrationBuilder.InsertData(
+                table: "ALUNOS",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 2, "manuelbueno@yahoo.com", 22, "Manuel Bueno" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ALUNOS");
+        }
+    }
+}
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
 
   const [data, setData] = useState([]);
   const [modalIncluir, setModalIncluir] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
 
   const [alunoSelecionado, setAlunoSelecionado] = useState({
     id: '',
@@ -20,6 +21,10 @@ function App() {
 
   const abrirFecharModalIncluir = () => {
     setModalIncluir(!modalIncluir);
+  }
+
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar);
   }
 
   const handleChange = e => {
@@ -50,6 +55,30 @@ function App() {
       })
   }
 
+  const pedidoPut = async () => {
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.put(baseUrl + "/" + alunoSelecionado.id, alunoSelecionado)
+      .then(response => {
+        var resposta = response.data;
+        var dadosAuxiliar = data;
+        dadosAuxiliar.map(aluno => {
+          if (aluno.id === alunoSelecionado.id) {
+            aluno.nome = resposta.nome;
+            aluno.email = resposta.email;
+            aluno.idade = resposta.idade;
+          }
+        });
+        abrirFecharModalEditar();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  const selecionarAluno = (aluno, opcao) => {
+    setAlunoSelecionado(aluno);
+    (opcao === "Editart") && abrirFecharModalEditar();
+  }
+
   useEffect(() => {
     pedidoGet();
   })
@@ -61,7 +90,7 @@ function App() {
       <header>
         <div className='aline-cadastro'>
           <img className='img' src={logoCadastro} alt="Cadastro" />
-          <button className="btn btn-success" onClick={()=>abrirFecharModalIncluir()}>Incluir Novo Aluno</button>
+          <button className="btn btn-success" onClick={() => abrirFecharModalIncluir()}>Incluir Novo Aluno</button>
         </div>
       </header>
       <table className="table table-bordered">
@@ -82,8 +111,8 @@ function App() {
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
               <td>
-                <button className="btn btn-primary">Editar</button> {" "}
-                <button className="btn btn-danger">Excluir</button>
+                <button className="btn btn-primary" onClick={() => selecionarAluno(aluno, "Editart")}>Editar</button> {" "}
+                <button className="btn btn-danger" onClick={() => selecionarAluno(aluno, "Excluir")}>Excluir</button>
               </td>
             </tr>
           ))}
@@ -109,6 +138,35 @@ function App() {
         <ModalFooter>
           <button className="btn btn-primary" onClick={() => pedidoPost()}>Incluir</button>{" "}
           <button className="btn btn-danger" onClick={() => abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>Incluir Alunos</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome</label>
+            <label>ID:</label>
+            <br />readOnly value={alunoSelecionado && alunoSelecionado.id}
+            <br />
+            <label>Nome:</label>
+            <input type="text" className="form-control" name="nome" onChange={handleChange} 
+            value={alunoSelecionado && alunoSelecionado.nome}/><br/>
+            <br />
+            <label>Email</label>
+            <br />
+            <input type="text" className="form-control" name="email" onChange={handleChange} 
+            value={alunoSelecionado && alunoSelecionado.email}/>  <br/>
+            <label>Idade</label>
+            <br />
+            <input type="text" className="form-control" name="idade" onChange={handleChange} 
+            value={alunoSelecionado && alunoSelecionado.idade}/><br/>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPut()}>Editar</button>{" "}
+          <button className="btn btn-danger" onClick={() => abrirFecharModalEditar()}>Cancelar</button>
         </ModalFooter>
       </Modal>
     </div>
