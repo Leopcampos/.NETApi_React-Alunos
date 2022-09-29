bd9b4eeff7df255ca1dfbc035e9e8b8b85313277
Leo Campos <leopcampos85@gmail.com>
2022-09-29 10:30:31 -0300

    Implantando o Login na página inicial da aplicação

--- Frontend/clientreact/src/pages/Alunos/index.js
+++ Frontend/clientreact/src/pages/Alunos/index.js
@@ -1,19 +1,55 @@
-import React from "react";
-import './styles.css';
-import logoCadadstro from '../../assets/cadastro.png';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FiEdit, FiUserX, FiXCircle } from 'react-icons/fi';
+import './styles.css';
+
+import logoCadadstro from '../../assets/cadastro.png';
+import api from '../../services/api';
+
 
 export default function Alunos() {
+
+    const[nome, setNome] = useState('');
+     const [alunos, setAlunos] = useState([]);
+
+     const email = localStorage.getItem('email');
+     const token = localStorage.getItem('token');
+   
+     const authorization = {
+         headers : {
+           Authorization : `Bearer ${token}`
+         }
+     }
+
+     const navigate = useNavigate();
+
+    useEffect( ()=> {
+        api.get('api/alunos',authorization).then(
+          response=> {setAlunos(response.data);
+         }, token)
+      })
+
+      async function logout(){
+        try{
+            localStorage.clear();
+            localStorage.setItem('token','');
+            authorization.headers = '';
+            navigate('/');
+        }
+        catch(err){
+            alert("Não foi possível realizar o logout" + err);
+        }
+      }
+
     return (
         <div className="aluno-container">
             <header>
                 <img src={logoCadadstro} alt="Cadastro" />
-                <span>Bem-Vindo, <strong>Leonardo</strong>!</span>
+                <span>Bem-Vindo, <strong>{email}!</strong>!</span>
                 <Link className="button" to="/aluno/novo/0">Novo Aluno</Link>
-                <button type="button">
-                    x
-                    {/* <FiXCircle size={35} color="#17202a" /> */}
+                <button onClick={logout} type="button">
+                    
+                    <FiXCircle size="35" color="#17202a" />
                 </button>
             </header>
             <form>
@@ -22,30 +58,24 @@ export default function Alunos() {
                     Filtrar aluno por nome (parcial)
                 </button>
             </form>
+
             <h1>Relação de Alunos</h1>
             <ul>
-                <li>
-                    <b>Nome:</b> Paulo<br /><br />
-                    <b>Email:</b> paulo@email.com<br /><br />
-                    <b>Idade:</b> 22<br /><br />
-                    <button type="button">
-                        <FiEdit size={25} color="#17202a" />
-                    </button>
-                    <button type="button">
-                        <FiUserX size={25} color="#17202a" />
-                    </button>
-                </li>
-                <li>
-                    <b>Nome:</b> Cícero<br /><br />
-                    <b>Email:</b> cicero@email.com<br /><br />
-                    <b>Idade:</b> 50<br /><br />
-                    <button type="button">
-                        <FiEdit size={25} color="#17202a" />
-                    </button>
-                    <button type="button">
-                        <FiUserX size={25} color="#17202a" />
-                    </button>
-                </li>
+                {alunos.map(aluno => (
+                    <li key={aluno.id}>
+                        <b>Nome:</b>{aluno.nome}<br /><br />
+                        <b>Email:</b>{aluno.email}<br /><br />
+                        <b>Idade:</b>{aluno.idade}<br /><br />
+
+                        <button type="button">
+                            <FiEdit size="25" color="#17202a" />
+                        </button>
+
+                        <button type="button" >
+                            <FiUserX size="25" color="#17202a" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
--- Frontend/clientreact/src/pages/Login/index.js
+++ Frontend/clientreact/src/pages/Login/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
 import './styles.css';
-import logoImagem from '../../assets/login4.png';
+import logoImage from '../../assets/login4.png';
+import api from '../../services/api';
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function login(event){
+        event.preventDefault();
+
+        const data = {
+            email, password
+    };
+
+    try {
+
+        const response = await api.post('https://localhost:44373/api/Account/LoginUser', data)
+
+        localStorage.setItem('email', email);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('expiration', response.data.expiration);
+
+        navigate('/alunos');
+
+    }catch(error){
+        alert("O login falhou " + error)
+    }
+
+    }
+
     return (
         <div className="login-container">
             <section className="form">
-                <img src={logoImagem} alt="Login" id="img1" />
-                <form>
+                <img src={logoImage} alt="login" id="img1" />
+                <form onSubmit={login}>
                     <h1>Cadastro de Alunos</h1>
-                    <input placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button class="button" type="submit">Login</button>
+                    
+                    <input placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} />
+
+                    <input type="password" placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)} />
+
+                    <button className="button" type="submit">Login</button>
                 </form>
             </section>
         </div>
